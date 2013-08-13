2f3934b2d2fbe13faf1b67d4eab6dd1f3961a743
Barney Hanlon <shrikeh@gmail.com>
2013-08-13 10:24:29 +0100

    Initial commit

---
+++ src/Hal/Symfony/Response/JsonResponse.php
@@ -0,0 +1,13 @@
+<?php
+namespace Shrikeh\Hal\Symfony\Response;
+
+use \NoCarrier\Hal;
+use \Symfony\Component\HttpFoundation\JsonResponse as SymfonyJsonResponse;
+
+class JsonResponse extends SymfonyJsonResponse
+{
+    public function __construct()
+    {
+
+    }
+}
---
+++ src/Hal/Symfony/Response/XmlResponse.php
@@ -0,0 +1,10 @@
+<?php
+namespace Shrikeh\Hal\Symfony\Response;
+
+use \NoCarrier\Hal;
+use \Symfony\Component\HttpFoundation\Response;
+
+class XmlResponse extends Response
+{
+
+}
